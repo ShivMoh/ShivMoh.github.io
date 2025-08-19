f509bcf5515afea0beddc21578f3b6ff246ff0b0
shivmoh <shiveshmohamed1234@gmail.com>
2025-08-19 07:49:00 -0400

    init

--- personal/next.config.ts
+++ personal/next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  assetPrefix: "https://shivmoh.github.io",
+  distDir: 'dist',
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
